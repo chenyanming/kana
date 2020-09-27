4a01b1a1f4288800dedf8a1e2952cf6953ffa72c
chenyanming <elecming@gmail.com>
2020-09-27 15:38:30 +0800

    Delete kanji-mode as dependency.

--- kana.el
+++ kana.el
@@ -7,7 +7,9 @@
 ;; Keywords: tools
 ;; Created: 23 May 2020
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "24.4") (kanji-mode "1.0"))
+;; Package-Requires: ((emacs "24.4"))
+
+;; This file is NOT part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
